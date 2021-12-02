02a0faf78bd411a2ab7ebf60134fdeb6a4d91399
sebas <sebaslpez0519@gmail.com>
2021-12-01 21:16:31 -0500

    Creacion de paquetes

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MenuNoLogin,MenuLogin} from '../Structures/Menus'
+
+function NavBar(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/Structures/Menus.js
@@ -0,0 +1,21 @@
+export const MenuNoLogin=[
+    {
+        titulo: 'Home',
+        url:'/'
+    }
+];
+
+export const MenuLogin=[
+    {
+        titulo:'Mis mascotas',
+        url:'/Mis_Mascotas'
+    },
+    {
+        titulo:'Citas',
+        url:'/Citas'
+    },
+    {
+        titulo:'Perfil',
+        url:'/Perfil'
+    }
+]
