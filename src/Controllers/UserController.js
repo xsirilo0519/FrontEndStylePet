f94f25c7b2596bdb87283700cacfd2aa58f48a5f
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-02 15:24:59 -0500

    Se crea el UserController

---
+++ src/Controllers/UserController.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const HOST_API_LOGIN = "http://localhost:8080/Usuario/Login";
+
+
+
+const Login = () => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default Login;
